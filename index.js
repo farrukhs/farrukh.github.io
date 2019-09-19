d33c85e3313fcda9db5f414d5e157f7c6036ec55
farrukhs <farrukh.s.siddiqi@gmail.com>
2019-09-19 11:39:20 +0100

    commit

--- index.js
+++ index.js
@@ -2,8 +2,12 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT ||3000;
 app.listen(port, () => console.log(`listening on port ${port}`));
-app.use(express.static('public'));
+app.use(express.static(__dirname + '/public'));
 app.use(express.json({limit: '1mb'}));
+
+// app.get('/', function(req, res){
+//    res.redirect('/undex.html');
+// });
 
 app.post('/api', (request, response) => {
     console.log(request.body);
